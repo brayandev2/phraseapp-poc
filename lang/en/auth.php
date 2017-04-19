da477a2c0744fc637f61d44afb0c4bf7ccc46f2c
Brayan Dichtl <brayan.dichtl@olx.com>
2017-04-19 14:09:26 +0100

    new foo key

--- lang/en/auth.php
+++ lang/en/auth.php
@@ -2,4 +2,5 @@
 return array(
 	"failed" => "These credentials do not match our records.",
 	"throttle" => "Too many login attempts. Please try again in :seconds seconds.",
+    "new-foo-key" => "Your translation goes here."
 );
